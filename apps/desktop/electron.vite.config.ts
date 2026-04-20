7679f34c7850f8623f0524f2b7aebfb3a062f908
PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
2026-04-20 20:36:42 +0200

    feat(desktop): load plugin from TypeScript file (#29)

--- apps/desktop/electron.vite.config.ts
+++ apps/desktop/electron.vite.config.ts
@@ -5,7 +5,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   main: {
     envDir: "../../",
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: `${import.meta.dirname}/src/main/index.ts`,
+          "plugin-ts-runner": `${import.meta.dirname}/src/main/plugin-ts-runner.ts`
+        },
+        output: {
+          entryFileNames: "[name].js"
+        }
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
